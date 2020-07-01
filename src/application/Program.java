2727c371a7d1ee4ddf747e6044ea2d485e8c2f18
Evaldo Ribeiro <evardis@gmail.com>
2020-07-01 00:00:05 -0300

    Projeto Predicate-3

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,7 +17,7 @@ public class Program {
 		list.add(new Product("Tablet", 350.0));
 		list.add(new Product("HD Case", 80.0));
 		
-		list.removeIf(new ProductPredicate());
+		list.removeIf(Product::staticProductPredicate);
 		
 		for (Product p : list) {
 			System.out.println(p);	
