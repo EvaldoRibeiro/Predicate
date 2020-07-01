31713e68b22118b406870dbeb26f1a0a8387523e
Evaldo Ribeiro <evardis@gmail.com>
2020-07-01 00:37:16 -0300

    Projeto Predicate-9 Lambda

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -31,7 +31,9 @@ public class Product {
 	public static void staticPriceUpdate(Product p) {
 		p.setPrice(p.getPrice() * 1.1);
 	}
-	
+	public void nonStaticPriceUpdate() {
+		setPrice(getPrice() * 1.1); // price = price * 1.1
+	}
 	@Override
 	public String toString() {
 		return name + ", " + String.format("%.2f" , price);
